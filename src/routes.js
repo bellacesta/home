a2a28cfbc40b75ae78aa9238bea45d5ab6c76cd4
bellacesta <sibellyribeiro08@gmail.com>
2023-08-10 09:54:24 -0300

    substituindo Browserouter por Hashrouter dentro das rountes

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 
 function RoutesApp() {
     return (
-        <BrowserRouter>
+            <HashRouter>
             <Routes>
-
                 <Route path="/" element={<Home />} />
-          
             </Routes>
-
-        </BrowserRouter>
+            </HashRouter>
 
     )
 
